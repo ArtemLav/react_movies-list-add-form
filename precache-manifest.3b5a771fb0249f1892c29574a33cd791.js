dc8aab199873494076f7b419806e2e9b77fbcd60
Artem Lavreniuk <artemlavrenyuk@gmail.com>
2020-10-30 13:29:09 +0200

    Updates

---
+++ precache-manifest.3b5a771fb0249f1892c29574a33cd791.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f34231013b6c94a3456eaf2d60068b32",
+    "url": "./index.html"
+  },
+  {
+    "revision": "75b2a8b8c02ee217ed3a",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "a4b9ed9bdc9e742fd9bc",
+    "url": "./static/css/main.5b5eb71e.chunk.css"
+  },
+  {
+    "revision": "75b2a8b8c02ee217ed3a",
+    "url": "./static/js/2.f0d7f16a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f0d7f16a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4b9ed9bdc9e742fd9bc",
+    "url": "./static/js/main.5b964ed7.chunk.js"
+  },
+  {
+    "revision": "3cfb45b61c015a055433",
+    "url": "./static/js/runtime-main.0527da25.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2088c32903613710f8f9678a01b40cd4.js"
+  "./precache-manifest.3b5a771fb0249f1892c29574a33cd791.js"
 );
 
 self.addEventListener('message', (event) => {
